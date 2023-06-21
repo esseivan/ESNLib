449327f9b07c20ac029f2856115401e8b30ae4f0
ESN <nicolas.esseiva@gmail.com>
2023-06-21 20:45:22 +0200

    csvImport class. (failed test for now)

---
+++ ESNLib.Tools.WinForms/CsvImportAs.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESNLib.Tools.WinForms
+{
+    /// <summary>
+    /// Import a csv file into a list of objects. Let the user decide the columns
+    /// </summary>
+    public class CsvImportAs<T> where T : new()
+    {
+
+
+        public void Import()
+        {
+
+        }
+
+        public List<T> ImportData(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                return null;
+            }
+
+            // Split by lines
+            string[] lines = data.Split('\n');
+
+            // Headers on first line
+            string[] headers = lines[0].Split(',');
+            lines = lines.Skip(1).ToArray();
+
+            // Generate Properties Info
+            List<PropertyInfo> properties = new List<PropertyInfo>();
+
+            foreach (string header in headers)
+            {
+                var propInfo = typeof(T).GetProperty(header);
+                properties.Add(propInfo);
+            }
+
+            // Generate T list
+            List<T> list = new List<T>();
+
+            foreach (string line in lines)
+            {
+                if(string.IsNullOrEmpty(line))
+                { continue; }
+
+                T item = new T();
+                string[] lineItems = line.Split(',');
+
+                for (int i = 0; i < lineItems.Length; i++)
+                {
+                    if (properties[i] == null) { continue; }
+
+                    string lineItem = lineItems[i];
+                    properties[i].SetValue(item, lineItem);
+                }
+                list.Add(item);
+            }
+
+            return list;
+        }
+
+    }
+}
---
+++ ESNLib.Tools.WinFormsTests/CsvImportAsTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESNLib.Tools.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESNLib.Tools.WinForms.Tests
+{
+    [TestClass()]
+    public class CsvImportAsTests
+    {
+        private string GenerateCSV(MyClass item)
+        {
+            string csvOutput = $"dummy2,x,n,y,dummy2,text\n";
+            csvOutput += $"DummyText,{item.x},{item.n},{item.y},DummyText2,{item.text}\n";
+
+            return csvOutput;
+        }
+
+        private string GenerateCSV(IEnumerable<MyClass> classes)
+        {
+            string csvOutput = $"dummy2,x,n,y,dummy2,text\n";
+
+            foreach (MyClass item in classes)
+            {
+                csvOutput += $"DummyText,{item.x},{item.n},{item.y},DummyText2,{item.text}\n";
+            }
+
+            return csvOutput;
+        }
+
+        /// <summary>
+        /// Template class for testing purposes
+        /// </summary>
+        private class MyClass
+        {
+            public int x { get; set; }
+            public int y;
+
+            public float n;
+
+            public string text;
+        }
+
+
+        [TestMethod()]
+        public void TestImport()
+        {
+            // CSV data
+            MyClass c = new MyClass()
+            {
+                x = 1,
+                y = 2,
+                n = 3.0f,
+                text = "name",
+            };
+            string s = GenerateCSV(c);
+
+            CsvImportAs<MyClass> csvi = new CsvImportAs<MyClass>();
+            List<MyClass> result = csvi.ImportData(s);
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(1, result[0].x);
+            Assert.AreEqual(2, result[0].y);
+            Assert.AreEqual(3.0, result[0].n);
+            Assert.AreEqual("name", result[0].text);
+        }
+    }
+}
